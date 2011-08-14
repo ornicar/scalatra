dc09d5c67a0482d5a0d35a12db56562f3afe0bf5
ornicar <thibault.duplessis@gmail.com>
2011-08-14 15:32:55 -0700

    Implement RouteRegistry.toString, add route tests

--- core/src/main/scala/org/scalatra/Route.scala
+++ core/src/main/scala/org/scalatra/Route.scala
@@ -5,13 +5,15 @@ import util.MultiMap
 
 case class Route(routeMatchers: Iterable[RouteMatcher], action: Action) {
   def apply(): Option[MatchedRoute] = {
-    routeMatchers.foldLeft(Option(MultiMap())) { 
-      (acc: Option[MultiParams], routeMatcher: RouteMatcher) => for { 
+    routeMatchers.foldLeft(Option(MultiMap())) {
+      (acc: Option[MultiParams], routeMatcher: RouteMatcher) => for {
         routeParams <- acc
-        matcherParams <- routeMatcher() 
+        matcherParams <- routeMatcher()
       } yield routeParams ++ matcherParams
     } map { routeParams => MatchedRoute(action, routeParams) }
   }
+
+  override def toString: String = routeMatchers mkString " "
 }
 
 case class MatchedRoute(action: Action, multiParams: MultiParams)
